dd76349009d6eb225d9d45333e4910c772fd7c2a
gaay4229 <mosharafhilwa@gmail.com>
2023-05-22 11:46:43 +0300

    arrays function

--- Arrays/main.js
+++ Arrays/main.js
@@ -1,1 +1,41 @@
-console.log("HI");
+// let fruits = ['Mangoes', 'banana', 'Pawpaw'];
+
+// // console.log(fruits[2]);
+// // console.log(fruits.length);
+
+// // push means to add new value in the back
+// // adding back
+// fruits.push("apple");
+// console.log(fruits);
+
+
+
+// // pop means to remove the value that was in the back
+// // removing back
+// fruits.pop();
+// console.log(fruits);
+
+
+// // unshift means to add new value in front
+// // adding front
+// fruits.unshift("apple");
+// console.log(fruits);
+
+// // unshift means to remove new value in front
+// // removing front
+// fruits.shift();
+// console.log(fruits);
+
+// function kaan waxa uu naga saacidaa maadaama value walba 
+//aa hal halmar u soo daabici laheen asiga kuli ayaa
+// hal mar daabacan kartaa waa na function ka forEach.
+// Variable.forEach(function(value){
+
+// }) Tussel waa dhax ah waa kan.
+
+// let fruits = ['Mangoes', 'banana', 'Pawpaw', 'lows', 'fruits', ];
+//  fruits.forEach(function(item){
+//         console.log(item);
+//  })
+
+ 
