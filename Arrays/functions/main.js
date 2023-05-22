0f6f79a8c279dfc95887eda66c903427ae382306
gaay4229 <mosharafhilwa@gmail.com>
2023-05-22 11:47:39 +0300

    arrays function

---
+++ Arrays/functions/main.js
@@ -0,0 +1,40 @@
+// function of difinition or fuction of declaration
+
+// function add(num){
+//     return num + num;
+// }
+// console.log(add(10));
+// console.log(add(20));
+// console.log(add(30));
+// console.log(add(40));
+
+// function sqaure(num){
+//     return num * num;
+
+// }
+// console.log(sqaure(10));
+// console.log(sqaure(20));
+// console.log(sqaure(30));
+// console.log(sqaure(40));
+
+// functon kaan waxa uu naga caabinaa in aan howsha aan ku dheeraano.
+
+
+// same functions this is we call function expression
+
+// const sqaure = function(num){
+//     return num * num;
+// }
+// console.log(sqaure(4));
+
+// Arrow FUnctio 
+// one way when we use one parameter
+// const sqaure = num =>  num * num;
+// console.log(sqaure(4))
+// console.log(sqaure(7))
+
+
+// Arrow function 
+// another way that when we use two parameter
+// const add = (num1, num2) => num1 * num2;
+// console.log(add(3,3));
